017fd8e867097e1ec798a2b41dbbef161e5da144
MD NAIM UDDIN <148058479+naim5641@users.noreply.github.com>
2025-09-02 08:00:17 +0600

    Add files via upload

---
+++ SQL_DAY_01.sql
@@ -0,0 +1,43 @@
+-- Create the employees table
+CREATE TABLE employees (
+    employee_id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    department VARCHAR(50),
+    salary DECIMAL(10, 2)
+);
+
+-- Insert records for three departments
+INSERT INTO employees (name, department, salary) VALUES 
+('John Doe', 'Engineering', 63000),
+('Jane Smith', 'Engineering', 55000),
+('Michael Johnson', 'Engineering', 64000),
+('Emily Davis', 'Marketing', 58000),
+('Chris Brown', 'Marketing', 56000),
+('Emma Wilson', 'Marketing', 59000),
+('Alex Lee', 'Sales', 58000),
+('Sarah Adams', 'Sales', 58000),
+('Ryan Clark', 'Sales', 61000);
+
+
+/*
+
+Write the SQL query to find the second highest salary
+
+*/
+
+-- -------------------------
+-- My Solution
+-- -------------------------
+
+
+
+select * from employees
+order by salary
+
+select 
+	max(salary) as sencond_higest
+from 
+	employees
+where (salary < (select max(salary) from employees))
+
+
