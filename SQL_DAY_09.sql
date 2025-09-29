e9fa09fc9fb9c654253b4f9527a692fb2c92251c
MD NAIM UDDIN <148058479+naim5641@users.noreply.github.com>
2025-09-29 14:30:15 +0600

    Add files via upload

---
+++ SQL_DAY_09.sql
@@ -0,0 +1,62 @@
+
+-- Create Customers table
+DROP TABLE IF EXISTS customers;
+CREATE TABLE Customers (
+    CustomerID INT,
+    CustomerName VARCHAR(50)
+);
+
+-- Create Purchases table
+DROP TABLE IF EXISTS purchases;
+CREATE TABLE Purchasess (
+    PurchaseID INT,
+    CustomerID INT,
+    ProductName VARCHAR(50),
+    PurchaseDate DATE
+);
+
+-- Insert sample data into Customers table
+INSERT INTO Customers (CustomerID, CustomerName) VALUES
+(1, 'John'),
+(2, 'Emma'),
+(3, 'Michael'),
+(4, 'Ben'),
+(5, 'John')	;
+
+-- Insert sample data into Purchases table
+INSERT INTO Purchasess (PurchaseID, CustomerID, ProductName, PurchaseDate) VALUES
+(100, 1, 'iPhone', '2024-01-01'),
+(101, 1, 'MacBook', '2024-01-20'),	
+(102, 1, 'Airpods', '2024-03-10'),
+(103, 2, 'iPad', '2024-03-05'),
+(104, 2, 'iPhone', '2024-03-15'),
+(105, 3, 'MacBook', '2024-03-20'),
+(106, 3, 'Airpods', '2024-03-25'),
+(107, 4, 'iPhone', '2024-03-22'),	
+(108, 4, 'Airpods', '2024-03-29'),
+(110, 5, 'Airpods', '2024-02-29'),
+(109, 5, 'iPhone', '2024-03-22');
+
+
+
+select
+	*
+from Customers as c
+join Purchasess as p
+on c.customerid = p.customerid
+join Purchasess as p1
+on c.customerid = p1.customerid
+where p.productname = 'iPhone'
+and
+p1.productname = 'Airpods'
+and
+p.purchasedate < p1.purchasedate
+
+
+
+
+
+
+
+
+
